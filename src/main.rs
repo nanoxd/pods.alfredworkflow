a6e04c2ada113f7f3fb3a2f727bc3d91924a950e
Fernando Paredes <nano@fdp.io>
2018-09-16 17:09:49 -0400

    ♻️ Update Search URL and JSON serialization

--- src/main.rs
+++ src/main.rs
@@ -7,79 +7,83 @@ extern crate serde_json;
 extern crate quicli;
 #[macro_use]
 extern crate serde_derive;
-use quicli::prelude::*;
-use models::{Pod, SearchResponse};
 use alfred::{ItemBuilder, Modifier};
+use models::{AlgoliaResponse, Pod};
+use quicli::prelude::*;
+use std::collections::HashMap;
 use std::io;
 
 mod models;
 
-fn search_pods(query: &str) -> Result<Vec<Pod>> {
-  let url: &str = &format!("https://search.cocoapods.org/api/v1/pods.picky.hash.json?query={}&ids=10&offset=0&sort=quality", query);
-  let mut response: SearchResponse = reqwest::get(url)?.json()?;
-  let pods = response.into_allocation();
+const ALGOLIA_APPLICATION_ID: &'static str = "WBHHAMHYNM";
+const ALGOLIA_API_KEY: &'static str = "4f7544ca8701f9bf2a4e55daff1b09e9";
 
-  match pods {
-    Some(pods) => Ok(pods.pods()),
-    None => Err(format_err!("Invalid Response")),
-  }
+fn search_pods_v2(query: &str) -> Result<Vec<Pod>> {
+    let url: &str = &format!("https://wbhhamhynm-3.algolianet.com/1/indexes/cocoapods/query?x-algolia-application-id={}&x-algolia-api-key={}", ALGOLIA_APPLICATION_ID, ALGOLIA_API_KEY);
+    let client = reqwest::Client::new();
+
+    let mut map = HashMap::new();
+    let q = format!("query={}", query);
+    map.insert("params", q);
+
+    let response: AlgoliaResponse = client.post(url).json(&map).send()?.json()?;
+    Ok(response.hits)
 }
 
 fn pod_to_alfred_item(pods: Vec<Pod>, query: &str) -> io::Result<()> {
-  let items: Vec<_> = pods
-    .into_iter()
-    .map(|pod| {
-      ItemBuilder::new(pod.title())
-        .text_copy(pod.stanza())
-        .arg(pod.url())
-        .valid(true)
-        .subtitle(pod.summary)
-        .subtitle_mod(Modifier::Option, format!("Open Repo: {}", pod.source.git))
-        .arg_mod(Modifier::Option, pod.source.git)
-        .into_item()
-    })
-    .collect();
+    let items: Vec<_> = pods
+        .into_iter()
+        .map(|pod| {
+            ItemBuilder::new(pod.title())
+                .text_copy(pod.stanza())
+                .arg(pod.url())
+                .valid(true)
+                .subtitle(pod.summary)
+                .subtitle_mod(Modifier::Option, format!("Open Repo: {}", pod.source.git))
+                .arg_mod(Modifier::Option, pod.source.git)
+                .into_item()
+        }).collect();
 
-  if items.is_empty() {
-    no_items(query)
-  } else {
-    alfred::json::write_items(io::stdout(), &items)
-  }
+    if items.is_empty() {
+        no_items(query)
+    } else {
+        alfred::json::write_items(io::stdout(), &items)
+    }
 }
 
 fn no_items(query: &str) -> io::Result<()> {
-  let item = ItemBuilder::new("No Pods Found")
-    .subtitle("Open CocoaPods Search?")
-    .arg(format!("https://cocoapods.org/?q={}", query))
-    .into_item();
+    let item = ItemBuilder::new("No Pods Found")
+        .subtitle("Open CocoaPods Search?")
+        .arg(format!("https://cocoapods.org/?q={}", query))
+        .into_item();
 
-  alfred::json::write_items(io::stdout(), &[item])
+    alfred::json::write_items(io::stdout(), &[item])
 }
 
 fn placeholder_item() -> io::Result<()> {
-  let item = ItemBuilder::new("Search for your favorite pods")
-    .arg("https://cocoapods.org")
-    .into_item();
+    let item = ItemBuilder::new("Search for your favorite pods")
+        .arg("https://cocoapods.org")
+        .into_item();
 
-  alfred::json::write_items(io::stdout(), &[item])
+    alfred::json::write_items(io::stdout(), &[item])
 }
 
 #[derive(Debug, StructOpt)]
 struct Cli {
-  query: String,
+    query: String,
 }
 
 main!(|args: Cli| {
-  let query = &args.query;
+    let query = &args.query;
 
-  if query.is_empty() {
-    placeholder_item()?
-  } else {
-    let request = search_pods(query);
+    if query.is_empty() {
+        placeholder_item()?
+    } else {
+        let request = search_pods_v2(query);
 
-    match request {
-      Ok(req) => pod_to_alfred_item(req, query)?,
-      Err(_) => no_items(query)?,
-    };
-  }
+        match request {
+            Ok(req) => pod_to_alfred_item(req, query)?,
+            Err(_) => no_items(query)?,
+        };
+    }
 });
--- src/models.rs
+++ src/models.rs
@@ -1,103 +1,67 @@
 #[derive(Serialize, Deserialize, Debug, Default)]
-pub struct SearchResponse {
-  allocations: Vec<Allocation>,
-}
-
-impl SearchResponse {
-  pub fn into_allocation(&mut self) -> Option<Allocation> {
-    if self.allocations.is_empty() {
-      None
-    } else {
-      Some(self.allocations.remove(0))
-    }
-  }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-// Allocation is a struct to avoid creating a custom deserializer as the response
-// is array indexed
-pub struct Allocation(String, f32, u32, Vec<Vec<String>>, Vec<u8>, Vec<Pod>);
-
-impl Allocation {
-  pub fn pods(self) -> Vec<Pod> {
-    self.5
-  }
+pub struct AlgoliaResponse {
+    pub hits: Vec<Pod>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Pod {
-  pub id: String,
-  pub version: String,
-  pub summary: String,
-  pub link: String,
-  pub source: Source,
+    pub name: String,
+    pub version: String,
+    pub summary: String,
+    pub homepage: String,
+    pub source: Source,
 }
 
 impl Pod {
-  pub fn title(&self) -> String {
-    format!("{} ({})", self.id, self.version)
-  }
+    pub fn title(&self) -> String {
+        format!("{} ({})", self.name, self.version)
+    }
 
-  pub fn url(&self) -> String {
-    format!("https://cocoapods.org/pods/{}", self.id)
-  }
+    pub fn url(&self) -> String {
+        format!("https://cocoapods.org/pods/{}", self.name)
+    }
 
-  pub fn stanza(&self) -> String {
-    format!("pod '{}', '~> {}'", self.id, self.version)
-  }
+    pub fn stanza(&self) -> String {
+        format!("pod '{}', '~> {}'", self.name, self.version)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Source {
-  pub git: String,
+    pub git: String,
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  fn test_pod() -> Pod {
-    Pod {
-      id: "RxSwift".to_string(),
-      version: "4.2.0".to_string(),
-      summary: "RxSwift is a ReactiveX ...".to_string(),
-      link: "https://github.com/ReactiveX/RxSwift".to_string(),
-      source: Source {
-        git: "https://github.com/ReactiveX/RxSwift".to_string(),
-      },
+    fn test_pod() -> Pod {
+        Pod {
+            name: "RxSwift".to_string(),
+            version: "4.2.0".to_string(),
+            summary: "RxSwift is a ReactiveX ...".to_string(),
+            homepage: "https://github.com/ReactiveX/RxSwift".to_string(),
+            source: Source {
+                git: "https://github.com/ReactiveX/RxSwift".to_string(),
+            },
+        }
     }
-  }
 
-  #[test]
-  fn test_into_allocation() {
-    let mut response = SearchResponse::default();
-    assert!(response.into_allocation().is_none());
+    #[test]
+    fn test_pod_url() {
+        let pod = test_pod();
+        assert_eq!(pod.url(), "https://cocoapods.org/pods/RxSwift");
+    }
 
-    let mut valid_response = SearchResponse {
-      allocations: vec![Allocation("".to_string(), 0.0, 0, vec![], vec![], vec![])],
-    };
+    #[test]
+    fn test_pod_stanza() {
+        let pod = test_pod();
+        assert_eq!(pod.stanza(), "pod 'RxSwift', '~> 4.2.0'");
+    }
 
-    let allocation = valid_response.into_allocation();
-    assert!(allocation.is_some());
-    let allocation = allocation.unwrap();
-    assert!(allocation.pods().is_empty());
-  }
-
-  #[test]
-  fn test_pod_url() {
-    let pod = test_pod();
-    assert_eq!(pod.url(), "https://cocoapods.org/pods/RxSwift");
-  }
-
-  #[test]
-  fn test_pod_stanza() {
-    let pod = test_pod();
-    assert_eq!(pod.stanza(), "pod 'RxSwift', '~> 4.2.0'");
-  }
-
-  #[test]
-  fn test_pod_title() {
-    let pod = test_pod();
-    assert_eq!(pod.title(), "RxSwift (4.2.0)");
-  }
+    #[test]
+    fn test_pod_title() {
+        let pod = test_pod();
+        assert_eq!(pod.title(), "RxSwift (4.2.0)");
+    }
 }
